352fd52b613a352d4e3089d60de0b52a5126a654
Hk <hk@Ages-MacBook-Pro-HK.local>
2022-03-14 23:04:10 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Projects from "./components/Projects/Projects";
+// import Footer from "./components/Footer";
+// import Resume from "./components/Resume/ResumeNew";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <Preloader load={load} /> */}
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/project" exact component={Projects} />
+          <Route path="/about" exact component={About} />
+          <Route path="/resume" exact component={Resume} />
+          <Redirect to="/" /> */}
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
